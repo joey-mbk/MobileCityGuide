aa36833646d0521b35294a2a16b38051399effc7
Simon Thibert <simon.thibert@student.uclouvain.be>
2012-05-03 16:48:31 +0200

    Ajout des Listeners sur les boutons de l'ecran d'accueil

--- src/com/mobilecityguide/MobileCityGuideActivity.java
+++ src/com/mobilecityguide/MobileCityGuideActivity.java
@@ -1,28 +1,44 @@
 package com.mobilecityguide;
 
+import com.mobilecityguide.R;
+import com.mobilecityguide.activity.Connect;
+import com.mobilecityguide.activity.Registrer;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MobileCityGuideActivity extends Activity {
-	
-	//On déclare toutes les variables dont on aura besoin
-	Button button0;
-	Button button1;
-	Button button2;
-	Button button3;
+public class MobileCityGuideActivity extends Activity implements OnClickListener {
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrer);
-        
-     /* //On récupère tout les éléments de notre interface graphique grâce aux ID
-        button0 = (Button) findViewById(R.id.button0);
-        button1 = (Button) findViewById(R.id.button1);
-        button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);*/
-
+        setContentView(R.layout.main);
+        setListeners();
     }
+
+	private void setListeners() {
+        View myVisitsButton = findViewById(R.id.bconnect);
+        myVisitsButton.setOnClickListener(this);
+        View settingsButton = findViewById(R.id.bregistrer);
+        settingsButton.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		Intent intent;
+		switch (v.getId()) {
+		case R.id.bconnect:
+			intent = new Intent(this, Connect.class);
+			startActivity(intent);
+			break;
+		case R.id.bregistrer:
+			intent = new Intent(this, Registrer.class);
+			startActivity(intent);
+			break;
+		}
+	}
 }
---
+++ src/com/mobilecityguide/activity/Registrer.java
@@ -0,0 +1,14 @@
+package com.mobilecityguide.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mobilecityguide.R;
+
+public class Registrer extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.registrer);
+	}
+}
