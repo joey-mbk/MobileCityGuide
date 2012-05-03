63b4eea8f0d36810e824039f145e4ac874caf911
francgrim <francois.grimart@gmail.com>
2012-05-03 18:00:49 +0200

    modification de MobileCityGuideActivity pour GPSController

--- src/com/mobilecityguide/MobileCityGuideActivity.java
+++ src/com/mobilecityguide/MobileCityGuideActivity.java
@@ -2,23 +2,31 @@ package com.mobilecityguide;
 
 import java.util.ArrayList;
 
+import com.mobilecityguide.controllers.GPSController;
+import com.mobilecityguide.controllers.UserController;
 import com.mobilecityguide.datamappers.UserMapper;
 import com.mobilecityguide.exceptions.GatewayException;
 import com.mobilecityguide.exceptions.RecordSetException;
+import com.mobilecityguide.gateways.SQL.SQLGateway;
 import com.mobilecityguide.models.User;
 
 import android.app.Activity;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MobileCityGuideActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	public static GPSController gps_controller;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UserMapper userMapper = new UserMapper(this);
+        new SQLGateway(this);
+        UserController.userMapper = new UserMapper(this);
         User user = null;
         try {
-			user = userMapper.getUser("Maxime");
+			user = UserController.userMapper.getUser("Maxime");
 		} catch (Exception e) {
 			System.out.println("Error");
 			e.printStackTrace();
@@ -33,5 +41,7 @@ public class MobileCityGuideActivity extends Activity {
         } else {
         	System.out.println("NULL !!!!");
         }
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        gps_controller = new GPSController(locationManager);
     }
 }
