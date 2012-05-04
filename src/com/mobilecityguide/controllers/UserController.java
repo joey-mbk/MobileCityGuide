561def2824d9a8496feac817ae939b2f68ed4c8e
francgrim <francois.grimart@gmail.com>
2012-05-04 10:30:12 +0200

    correction  de addNewUser de UserController

--- src/com/mobilecityguide/controllers/UserController.java
+++ src/com/mobilecityguide/controllers/UserController.java
@@ -5,14 +5,7 @@ import java.util.ArrayList;
 import com.mobilecityguide.datamappers.UserMapper;
 import com.mobilecityguide.models.Itinerary;
 import com.mobilecityguide.models.User;
-/*
- * Pour créer un nouvel utilisateur:
- * 
- * User newUser = new User();
- * newUser.set...;
- * setActiveUser(newUser);
- * ( addUser(newUser); )  
- */
+
 public class UserController {
 
 	public static User activeUser = null;
@@ -23,43 +16,48 @@ public class UserController {
 		UserController.activeUser = activeUser;
 		userName = activeUser.getName();
 	}
-	
-	public void saveUser(){
+
+	public static void saveUser(){
 		userMapper.save(activeUser);
 		userMapper.setUserName(activeUser,userName);
 	}
-	
-	public void addUser(){
+
+	public static void addNewUser(User newUser,String name ,String age,String[]languages, ArrayList<String> userCategoryList){
+		newUser.setName(name);
+		newUser.setLanguage(languages);
+		newUser.setAge(age);
+		newUser.setUserCategoryList(userCategoryList);
+		setActiveUser(newUser);
 		userMapper.addUser(activeUser);
 	}
-	
-	public void delUser(){
+
+	public static void delActiveUser(){
 		userMapper.deleteUser(activeUser.getName());
 	}
-	
-    public void setUserName(String name){
-    	activeUser.setName(name);
-    }
-    
-	public void setUserAge(String age){
-	    activeUser.setAge(age);	
+
+	public static void setActiveUserName(String name){
+		activeUser.setName(name);
 	}
-	
-	public void setUserLanguages(String[]languages){
+
+	public static void setActiveUserAge(String age){
+		activeUser.setAge(age);	
+	}
+
+	public static void setActiveUserLanguages(String[]languages){
 		activeUser.setLanguage(languages);
 	}
-	
-	public void setUserItineraryList(ArrayList<Itinerary> userItineraryList){
+
+	public static void setActiveUserItineraryList(ArrayList<Itinerary> userItineraryList){
 		activeUser.setUserItineraryList(userItineraryList);
 	}
-	
-	public void setUserCategory(ArrayList<String> userCategoryList){
+
+	public static void setActiveUserCategory(ArrayList<String> userCategoryList){
 		activeUser.setUserCategoryList(userCategoryList);
 	}
-	
-	public ArrayList<String> getAllUsersNames(){
+
+	public static ArrayList<String> getAllUsersNames(){
 		return userMapper.getAllUsersNames();
-		 
+
 	}
-	
+
 }
