1bbb90932496053b8c5fe9346b9b767e9182ff20
Joey M <joey@sinf.be>
2012-05-07 12:35:17 +0200

    Début de refonte de l'UI

--- src/com/mobilecityguide/MobileCityGuideActivity.java
+++ src/com/mobilecityguide/MobileCityGuideActivity.java
@@ -4,7 +4,6 @@ import com.mobilecityguide.R;
 import com.mobilecityguide.activity.About;
 import com.mobilecityguide.activity.Connect;
 import com.mobilecityguide.activity.Help;
-import com.mobilecityguide.activity.Registrer;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,24 +19,24 @@ public class MobileCityGuideActivity extends Activity implements OnClickListener
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.connect);
 		setListeners();
 	}
 
-	//Méthode qui se déclenchera lorsque vous appuierez sur le bouton menu du téléphone
+	//Mï¿½thode qui se dï¿½clenchera lorsque vous appuierez sur le bouton menu du tï¿½lï¿½phone
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		//Création d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
+		//Crï¿½ation d'un MenuInflater qui va permettre d'instancier un Menu XML en un objet Menu
 		MenuInflater inflater = getMenuInflater();
-		//Instanciation du menu XML spécifier en un objet Menu
+		//Instanciation du menu XML spï¿½cifier en un objet Menu
 		inflater.inflate(R.layout.menu_2, menu);
 
 		return true;
 	}
 
-	//Méthode qui se déclenchera au clic sur un item
+	//Mï¿½thode qui se dï¿½clenchera au clic sur un item
 	public boolean onOptionsItemSelected(MenuItem item) {
-		//On regarde quel item a été cliqué grâce à son id et on déclenche une action
+		//On regarde quel item a ï¿½tï¿½ cliquï¿½ grï¿½ce ï¿½ son id et on dï¿½clenche une action
 		switch (item.getItemId()) {
 		case R.id.quit:
 			//Pour fermer l'application il suffit de faire finish()
@@ -48,35 +47,14 @@ public class MobileCityGuideActivity extends Activity implements OnClickListener
 	}
 
 	private void setListeners() {
-		View connectButton = findViewById(R.id.connect_button);
-		connectButton.setOnClickListener(this);
-		View registrerButton = findViewById(R.id.register_button);
-		registrerButton.setOnClickListener(this);
-		View aboutButton = findViewById(R.id.about_button);
-		aboutButton.setOnClickListener(this);
-		View helpButton = findViewById(R.id.help_button);
-		helpButton.setOnClickListener(this);
+		//View connectButton = findViewById(R.id.connect_button);
+		//connectButton.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
 		Intent intent;
 		switch (v.getId()) {
-		case R.id.connect_button:
-			intent = new Intent(this, Connect.class);
-			startActivity(intent);
-			break;
-		case R.id.register_button:
-			intent = new Intent(this, Registrer.class);
-			startActivity(intent);
-			break;
-		case R.id.help_button:
-			intent = new Intent(this, Help.class);
-			startActivity(intent);
-			break;
-		case R.id.about_button:
-			intent = new Intent(this, About.class);
-			startActivity(intent);
-			break;
+		
 		}
 	}
 }
