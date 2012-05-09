b61b1e5096e11b6eef61b5a29e10fe8b6bcdbb57
francgrim <francois.grimart@gmail.com>
2012-05-09 23:15:20 +0200

    choix de l'itinéraire

--- src/com/mobilecityguide/activity/ItinerariesList.java
+++ src/com/mobilecityguide/activity/ItinerariesList.java
@@ -1,5 +1,7 @@
 package com.mobilecityguide.activity;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +10,20 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.mobilecityguide.MobileCityGuideActivity;
 import com.mobilecityguide.R;
+import com.mobilecityguide.controllers.ItineraryController;
+import com.mobilecityguide.controllers.POIController;
+import com.mobilecityguide.controllers.UserController;
 
-public class ItinerariesList extends Activity implements OnClickListener {
+public class ItinerariesList extends Activity implements OnClickListener, OnItemClickListener {
 
+	private String[]itinerariesList;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +64,29 @@ public class ItinerariesList extends Activity implements OnClickListener {
 	private void setListeners() {
 		View addPoiButton = findViewById(R.id.create_itinerary);
 		addPoiButton.setOnClickListener(this);
+		
+		ListView itinerariesListView = (ListView)findViewById(R.id.list);
+		//Remplissage de la liste de nom des itinéraires
+		ArrayList<String> itinerariesArrayList;
+		try {
+			itinerariesArrayList = ItineraryController.getCityItinerariesTitles();
+			itinerariesList = new String[itinerariesArrayList.size()];
+			itinerariesArrayList.toArray(itinerariesList);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		itinerariesListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itinerariesList));
+		itinerariesListView.setOnItemClickListener(this);
+	}
+
+	public void onItemClick(AdapterView<?> arg0,View arg1, int arg2, long id) {
+		try {
+			//TODO Démarrer une visite
+			System.out.println((itinerariesList[(int) id]));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		startActivity(new Intent(this, PoisList.class));
 	}
 
 	public void onClick(View v) {
