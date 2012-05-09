f11b3672b91fff4737483cf4347862684a679a00
francgrim <francois.grimart@gmail.com>
2012-05-09 22:56:02 +0200

    choix de la ville + réarrangement

--- src/com/mobilecityguide/activity/Connect.java
+++ src/com/mobilecityguide/activity/Connect.java
@@ -20,8 +20,9 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Connect extends Activity implements OnClickListener {
-
+public class Connect extends Activity implements OnClickListener, OnItemClickListener {
+    
+	private String[]userNamesList;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -29,7 +30,7 @@ public class Connect extends Activity implements OnClickListener {
 		setContentView(R.layout.connect);
 		setListeners();
 	}
-	
+
 
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -51,29 +52,26 @@ public class Connect extends Activity implements OnClickListener {
 	private void setListeners() {
 		View createButton = findViewById(R.id.create_profile);
 		createButton.setOnClickListener(this);
-		
+
 		ListView userListView = (ListView)findViewById(R.id.list);
 		//Remplissage de la liste de nom d'utilisateur
 		ArrayList<String>userNamesArrayList = UserController.getAllUsersNames();
-		final String[]userNamesList = new String[userNamesArrayList.size()];
-	    userNamesArrayList.toArray(userNamesList);
-	    userListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,userNamesList));
-	    
-		userListView.setOnItemClickListener(new OnItemClickListener() {
-      		@Override
-			public void onItemClick(AdapterView<?> arg0,
-						View arg1, int arg2, long arg3) {
-      			try {
-      				System.out.println(userNamesList[(int) arg3]);
-					UserController.setActiveUser(UserController.getUser(userNamesList[(int) arg3]));
-					setContentView(R.layout.city); 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-      		}
-      		});
+		userNamesList = new String[userNamesArrayList.size()];
+		userNamesArrayList.toArray(userNamesList);
+		
+		userListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,userNamesList));
+		userListView.setOnItemClickListener(this);
 	}
 
+	public void onItemClick(AdapterView<?> arg0,View arg1, int arg2, long id) {
+		try {
+			UserController.setActiveUser(UserController.getUser(userNamesList[(int) id]));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		startActivity(new Intent(this, CitiesList.class));
+	}
+	
 	public void onClick(View v) {
 		Intent intent;
 		switch (v.getId()) {
