dcc9645baeecccdfc8899c8b4cdcdffd54403b6b
francgrim <francois.grimart@gmail.com>
2012-05-09 22:15:11 +0200

    choix de l'utilisateur

--- src/com/mobilecityguide/activity/Connect.java
+++ src/com/mobilecityguide/activity/Connect.java
@@ -1,7 +1,10 @@
 package com.mobilecityguide.activity;
 
+import java.util.ArrayList;
+
 import com.mobilecityguide.MobileCityGuideActivity;
 import com.mobilecityguide.R;
+import com.mobilecityguide.controllers.UserController;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -11,6 +14,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Connect extends Activity implements OnClickListener {
 
@@ -21,6 +29,7 @@ public class Connect extends Activity implements OnClickListener {
 		setContentView(R.layout.connect);
 		setListeners();
 	}
+	
 
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
@@ -42,6 +51,27 @@ public class Connect extends Activity implements OnClickListener {
 	private void setListeners() {
 		View createButton = findViewById(R.id.create_profile);
 		createButton.setOnClickListener(this);
+		
+		ListView userListView = (ListView)findViewById(R.id.list);
+		//Remplissage de la liste de nom d'utilisateur
+		ArrayList<String>userNamesArrayList = UserController.getAllUsersNames();
+		final String[]userNamesList = new String[userNamesArrayList.size()];
+	    userNamesArrayList.toArray(userNamesList);
+	    userListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,userNamesList));
+	    
+		userListView.setOnItemClickListener(new OnItemClickListener() {
+      		@Override
+			public void onItemClick(AdapterView<?> arg0,
+						View arg1, int arg2, long arg3) {
+      			try {
+      				System.out.println(userNamesList[(int) arg3]);
+					UserController.setActiveUser(UserController.getUser(userNamesList[(int) arg3]));
+					setContentView(R.layout.city); 
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+      		}
+      		});
 	}
 
 	public void onClick(View v) {
