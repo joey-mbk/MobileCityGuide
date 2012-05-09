00ca7830fb482fd303461a79eefa5b092ef97cec
francgrim <francois.grimart@gmail.com>
2012-05-09 22:16:30 +0200

    initialisation des mappers

--- src/com/mobilecityguide/MobileCityGuideActivity.java
+++ src/com/mobilecityguide/MobileCityGuideActivity.java
@@ -7,6 +7,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.mobilecityguide.activity.Connect;
+import com.mobilecityguide.controllers.CategoryController;
+import com.mobilecityguide.controllers.ItineraryController;
+import com.mobilecityguide.controllers.POIController;
+import com.mobilecityguide.controllers.UserController;
+import com.mobilecityguide.datamappers.CategoryMapper;
+import com.mobilecityguide.datamappers.ItineraryMapper;
+import com.mobilecityguide.datamappers.POIMapper;
+import com.mobilecityguide.datamappers.UserMapper;
 
 public class MobileCityGuideActivity extends Activity implements OnClickListener {
 
@@ -14,13 +22,19 @@ public class MobileCityGuideActivity extends Activity implements OnClickListener
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.poi_details);
+		
+		POIController.poiMapper = new POIMapper(this);
+		CategoryController.categoryMapper = new CategoryMapper(this);
+		ItineraryController.itineraryMapper = new ItineraryMapper(this);
+		UserController.userMapper = new UserMapper(this);
+		
+		setContentView(R.layout.main);
 		setListeners();
 	}
 
 	private void setListeners() {
-		//View enterButton = findViewById(R.id.contentLayout);
-		//enterButton.setOnClickListener(this);
+		View enterButton = findViewById(R.id.contentLayout);
+		enterButton.setOnClickListener(this);
 	}
 
 	public void onClick(View v) {
