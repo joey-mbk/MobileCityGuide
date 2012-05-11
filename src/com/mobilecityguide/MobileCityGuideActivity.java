674e31734b54d66a458dba74fb6c3521c1c9532b
Simon Thibert <simon.thibert@student.uclouvain.be>
2012-05-11 11:12:18 +0200

    temp

--- src/com/mobilecityguide/MobileCityGuideActivity.java
+++ src/com/mobilecityguide/MobileCityGuideActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.mobilecityguide.activity.Connect;
-import com.mobilecityguide.activity.CreateItinerary;
 import com.mobilecityguide.controllers.CategoryController;
 import com.mobilecityguide.controllers.ItineraryController;
 import com.mobilecityguide.controllers.POIController;
