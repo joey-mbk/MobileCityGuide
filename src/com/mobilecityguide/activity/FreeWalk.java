16c27d81d9cea2c51fcb4a663af48a75990c45c1
Jos Zigabe <lil-j-01@hotmail.com>
2012-05-11 14:52:18 +0200

    free walk

--- src/com/mobilecityguide/activity/FreeWalk.java
+++ src/com/mobilecityguide/activity/FreeWalk.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Point;
 import android.location.Address;
 import android.location.Criteria;
@@ -33,9 +34,13 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 import com.mobilecityguide.MobileCityGuideActivity;
 import com.mobilecityguide.R;
+import com.mobilecityguide.controllers.POIController;
+import com.mobilecityguide.controllers.UserController;
 import com.mobilecityguide.datamappers.MyOverLay;
+import com.mobilecityguide.models.POI;
 
 public class FreeWalk extends MapActivity{
 
@@ -43,33 +48,53 @@ public class FreeWalk extends MapActivity{
 
 
 	MapView mapView; 
-	MapController mc;
-	GeoPoint p, q;
+	MapController mapController;
+	GeoPoint points; 
+	int color = Color.GREEN;
 
-	class MapOverlay extends com.google.android.maps.Overlay
-	{
+	public class MapOverlay extends Overlay {
+		private GeoPoint data;  
+
+		public MapOverlay( GeoPoint item) {
+			data = item;
+		}
+
+		/* (non-Javadoc)
+		 * @see com.google.android.maps.Overlay#draw(android.graphics.Canvas, com.google.android.maps.MapView, boolean, long)
+		 */
+
 		@Override
-		public boolean draw(Canvas canvas, MapView mapView, 
-				boolean shadow, long when) 
-		{
-			super.draw(canvas, mapView, shadow);                   
-
-			//---translate the GeoPoint to screen pixels---
-			Point screenPts = new Point();
-			mapView.getProjection().toPixels(p, screenPts);
-
-			//---add the marker---
+		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,long when) {
+			Projection projection = mapView.getProjection();
+			if (shadow == false) {
+				Paint paint = new Paint();
+				paint.setAntiAlias(true);
+				Point point = new Point();
+				projection.toPixels(data, point);
+				paint.setColor(color);
+				paint.setStrokeWidth(10);
+				canvas.drawPoint((float) point.x, (float) point.y, paint);
+				/*---add the marker---
 			Bitmap bmp = BitmapFactory.decodeResource(
 					getResources(), R.drawable.star);            
-			canvas.drawBitmap(bmp, screenPts.x, screenPts.y-50, null);         
-			return true;
+			canvas.drawBitmap(bmp, point.x, point.y-50, null);        
+			return true;*/
+			}
+			return super.draw(canvas, mapView, shadow, when);
 		}
+
+		/* (non-Javadoc)
+		 * @see com.google.android.maps.Overlay#draw(android.graphics.Canvas, com.google.android.maps.MapView, boolean)
+		 */
+		@Override
+		public void draw(Canvas canvas, MapView mapView, boolean shadow) {
+
+			super.draw(canvas, mapView, shadow);
+		}       
 
 		@Override
 		public boolean onTouchEvent(MotionEvent event, MapView mapView) 
 		{  
-
-
 			//---when user lifts his finger---
 			if (event.getAction() == 1) {                
 				GeoPoint p = mapView.getProjection().fromPixels(
@@ -131,55 +156,67 @@ public class FreeWalk extends MapActivity{
 		mapView = (MapView) findViewById(R.id.myMapView1);
 
 
-		mc = mapView.getController();
-		String coordinates[] = {"1.352566007", "103.78921587"};
-		double lat = 50.670558;
-		double lng = 4.616135;
-		
-		double dest_lat = 50.667969; // the testing destination 
-		double dest_long = 4.591487;
+		mapController = mapView.getController();
 
-		/* Get user's location */
+		//city
+		String city = "Louvain-La-Neuve"; // ˆ enlever
+		UserController.setCity(city);
+
+		if(UserController.getCity()!=""){
+
+			POI[] poi = null;
+			try {
+
+				poi = POIController.getPOIOfCity();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			List<Overlay> listOfOverlays = null;
+			//color=Color.RED;
+			//MapOverlay userOverlay = new MapOverlay(currentUserLocation());
+			//listOfOverlays = mapView.getOverlays();
+			//listOfOverlays.add(userOverlay);
+			//color=Color.GREEN;
+			
+			for (POI poi2 : poi) {
+
+				points = new GeoPoint(
+						(int) (poi2.getLatitude() * 1E6), 
+						(int) (poi2.getLongitude() * 1E6));
+
+				MapOverlay mapOverlay = new MapOverlay(points);
+				listOfOverlays = mapView.getOverlays();
+				listOfOverlays.add(mapOverlay);
+
+			}
+		}
+
+		mapController.animateTo(points); //user normalement !!!!!!!!
+		mapController.setZoom(17); 
+
+
+		mapView.setSatellite(true);
+		mapView.displayZoomControls(true);
+	}
+
+	public GeoPoint currentUserLocation (){
+		
+		GeoPoint points = null;
+
 		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 		Criteria criteria = new Criteria();
 		criteria.setAccuracy(Criteria.ACCURACY_FINE);
 		criteria.setAltitudeRequired(false);
 		Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, true));
 
-		//double fromLat = lastKnownLocation.getLatitude();
-		//double fromLon = lastKnownLocation.getLongitude();
-
-		p = new GeoPoint(
-				(int) (lat * 1E6), 
-				(int) (lng * 1E6));
-		q = new GeoPoint(
-				(int) (dest_lat * 1E6), 
-				(int) (dest_long * 1E6));
-
-        
-		mc.animateTo(p);
-		mc.animateTo(q);
+		double fromLat = lastKnownLocation.getLatitude();
+		double fromLon = lastKnownLocation.getLongitude();
 		
+		points = new GeoPoint((int)fromLat, (int)fromLon);
 		
-		mc.setZoom(17); 
-
-		//---Add a location marker---
-		MapOverlay mapOverlay = new MapOverlay();
-		
-		List<Overlay> listOfOverlays = mapView.getOverlays();
-		listOfOverlays.clear();
-		listOfOverlays.add(mapOverlay); 
-
-		
-		//mapView.getOverlays().add(new MyOverLay(gp1,gp2,3,color)); 
-
-		mapView.setTraffic(true);
-		//mapView.setSatellite(true);
-		mapView.setClickable(true);
-
-		mapView.invalidate();
-
-
+		return points;
 	}
 
 	@Override
@@ -187,6 +224,36 @@ public class FreeWalk extends MapActivity{
 		// TODO Auto-generated method stub
 		return false;
 	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.layout.menu, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent intent;
+		switch (item.getItemId()) {
+		case R.id.quit:
+			intent = new Intent(this, MobileCityGuideActivity.class);
+			startActivity(intent);
+			return true;
+		case R.id.change_user:
+			intent = new Intent(this, Connect.class);
+			startActivity(intent);
+			return true;
+		case R.id.change_city:
+			intent = new Intent(this, CitiesList.class);
+			startActivity(intent);
+			return true;
+		case R.id.edit_profile:
+			intent = new Intent(this, MobileCityGuideActivity.class);
+			startActivity(intent);
+			return true;
+		}
+		return false;
+	}
+
 }
 
 
